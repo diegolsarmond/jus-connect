570ad03daf5bfa2377043400836bdd9c3084708a
Diego Armond <65828770+diegolsarmond@users.noreply.github.com>
2025-09-28 12:41:21 -0300

    feat: support HTML formatting in blog articles

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import animatePlugin from "tailwindcss-animate";
+import typographyPlugin from "@tailwindcss/typography";
 
 export default {
     darkMode: ["class"],
@@ -146,5 +147,5 @@ export default {
             },
         },
     },
-    plugins: [animatePlugin],
+    plugins: [animatePlugin, typographyPlugin],
 } satisfies Config;
